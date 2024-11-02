97c5f0a087487e3efc06189cc97fc3ad6221f1e8
Abhijeet Singh Rawat <rawatab2@gmail.com>
2024-11-03 00:27:34 +0530

    add collection inside a doc in firebase

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import Cards from "../components/Cards";
-import { Modal } from "antd";
+import { collection, addDoc } from "firebase/firestore";
 import AddExpensesModal from "../components/modals/AddExpensesModal";
 import AddIncomeModal from "../components/modals/AddIncomeModal";
+import { auth, db } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import toast from "react-hot-toast";
+import moment from "moment";
 
 const Dashboard = () => {
+
+    const [user] = useAuthState(auth);// destruct it as it contains a array and we want just user
 
     const [isExpenseModal,setIsExpenseModal]=useState(false);
     const [isIncomeModal,setIsIncomeModal]=useState(false);
@@ -23,13 +29,36 @@ const Dashboard = () => {
         setIsIncomeModal(false);
     }
 
-    function onFinish(values,type){
-        console.log(values+type);
+     
+    async function addTransaction(transaction){
+        try{
+            // Add a new document with a generated id.
+            const docRef = await addDoc(collection(db, `users/${user.uid}/transactions`),transaction);
+            console.log("Document written with ID: ", docRef.id);
+            toast.success("Transaction Added !")
+            handleExpenseModal();
+            handleIncomeModal();
+        }
+        catch(e){
+            toast.error("Unable to Add Transaction")
+        }
     }
+
+    
+    function onFinish(values,type){
+        const newTransaction={
+            type:type,
+            date:moment(values.date).format("YYYY-MM-DD"),
+            amount:parseFloat(values.amount),
+            tag:values.tag,
+            name:values.name
+        }
+        addTransaction(newTransaction);
+    };
 
     return ( 
         <div>
-
+            
             <Cards showExpenseModal={showExpenseModal} showIncomeModal={showIncomeModal}/>
 
             <AddExpensesModal isExpenseModal={isExpenseModal} handleExpenseModal={handleExpenseModal} onFinish={onFinish}/>
