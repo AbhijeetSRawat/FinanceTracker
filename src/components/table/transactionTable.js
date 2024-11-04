8def672da23537a1f96bdb6d9e66a07a3ab6e4ee
Abhijeet Singh Rawat <rawatab2@gmail.com>
2024-11-04 23:20:28 +0530

    export csv done

--- src/components/table/transactionTable.js
+++ src/components/table/transactionTable.js
@@ -1,7 +1,10 @@
 import { Table } from 'antd';
 import { useState } from 'react';
 import React from 'react';
-import { Flex, Radio } from 'antd';
+import { Flex, Radio, Button } from 'antd';
+import './transactionTable.css'
+import { unparse } from 'papaparse';
+import moment from 'moment';
 
 const TransactionTable = ({ transactions }) => {
 
@@ -54,18 +57,36 @@ const TransactionTable = ({ transactions }) => {
       }
     });
 
+    function exportCSV(){
+      // Specifying fields and data explicitly
+      var csv =unparse({
+        fields: ["date","name","amount","type","tag"],
+        data: transactions
+      });
+
+      const blob = new Blob([csv], {type: 'text/csv;charset=utf-8;'});
+      
+      const url=URL.createObjectURL(blob);
+      const link=document.createElement("a");
+      link.href=url;
+      link.download="transactions.csv";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+
     return (
       <div>
           <div className='flex flex-col lg:flex-row mt-4'>
               
               <div className='flex mb-2 p-2'>
                 <label htmlFor="SerachBar" className='text-2xl lg:pl-1 font-bold text-slate-800'>
-                  <img src="https://i.pinimg.com/736x/fa/0e/7b/fa0e7b992eff03c576727e95c746005c.jpg" className='w-[38px] rounded-s-md' alt="image" />
+                  <img src="https://i.pinimg.com/736x/fa/0e/7b/fa0e7b992eff03c576727e95c746005c.jpg" className='w-[38px] rounded-s-md' alt="Search" />
                 </label>
                 <input type="text" className='bg-gray-100 rounded-e-md p-1 w-[85vw] lg:w-[65vw]  shadow-lg' id="SearchBar" value={search} placeholder="Search By Name..." onChange={(e)=>setSearch(e.target.value)} />
               </div>
 
-              <Flex className='p-2 lg:w-[26vw]' vertical gap="middle">
+              <Flex className='p-2 lg:w-[30vw]'  vertical gap="middle">
                 
                 <Radio.Group
                   block
@@ -75,24 +96,30 @@ const TransactionTable = ({ transactions }) => {
                   buttonStyle="solid"
                   value={typeFilter}
                   onChange={(e)=>setTypeFilter(e.target.value)}
+                  className="custom-radio-group"
                 />
                 
               </Flex>
           </div>
           
-          <div className='p-2 flex flex-col lg:flex-row lg:justify-between w-[100vw]' >
-            <h2 className=' text-2xl pt-1 font-bold'>My Transactions</h2>
+          <div className='p-2 flex flex-col lg:flex-row ' >
+            <h2 className=' text-2xl pt-1 font-bold '>My Transactions</h2>
 
             
-            <Flex className='mt-2 mx-auto' vertical gap="middle" >
+            <Flex className='mt-2 lg:mx-auto w-[100vw] lg:w-[30vw]' vertical gap="middle" >
               
-              <Radio.Group defaultValue="nosort" onChange={(e)=>setSortKey(e.target.value)}>
-                <Radio.Button value="nosort">No Sort</Radio.Button>
-                <Radio.Button value="sortbyamount">Sort By Amount</Radio.Button>
-                <Radio.Button value="sortbydate">Sort By Date</Radio.Button>
+              <Radio.Group defaultValue="nosort" className="custom-radio-group "  onChange={(e)=>setSortKey(e.target.value)}>
+                <Radio.Button className='w-[23vw] lg:pl-10 lg:w-[8vw]' value="nosort">No Sort</Radio.Button>
+                <Radio.Button className='w-[40vw] pl-6 lg:w-[10vw]' value="sortbyamount">Sort by Amount</Radio.Button>
+                <Radio.Button className='w-[33vw] pl-6 lg:w-[8vw]' value="sortbydate">Sort by Date</Radio.Button>
                 
               </Radio.Group>
               
+            </Flex>
+
+            <Flex gap="small" className='mt-3 ' wrap>
+              <Button className='bg-black ' onClick={exportCSV} type="primary">Export CSV</Button>
+              <Button className='bg-black ' type="primary">Import CSV</Button>
             </Flex>
 
           </div>
