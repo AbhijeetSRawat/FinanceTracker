802b160039884b92c303fb399e8d72e11a4216d9
Abhijeet Singh Rawat <rawatab2@gmail.com>
2024-11-05 12:58:53 +0530

    reset balance button hidden

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,7 +7,7 @@ const Cards = ({showExpenseModal,showIncomeModal,income,expense,totalBalance}) =
             <Row className="flex flex-col lg:flex-row w-[100vw] p-2 lg:p-3 gap-2 lg:gap-8">
                 <Card className="lg:w-[31vw] shadow-lg" title="Current Balance">
                     <p className="mb-2 text-lg">&#8377; {totalBalance}</p>
-                    <button className="w-[100%] bg-black rounded-lg text-white h-[40px] font-semibold hover:bg-white border hover:border-black hover:text-black">Reset Balance</button>
+                    {/* <button className="w-[100%] bg-black rounded-lg text-white h-[40px] font-semibold hover:bg-white border hover:border-black hover:text-black">Reset Balance</button> */}
                 </Card>
 
                 <Card className="lg:w-[31vw] shadow-lg" title="Total Income">
